54c837fe04322be3efcf0bc34a3d8b4fda98dd77
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-29 15:17:39 +0100

    adds hero image and content

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [HeroSearchComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
