86630a6a3bb10c83afdcf2973dc36d84a830c722
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-29 21:06:36 +0100

    adds tool section and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { SignInWidgetComponent } from './sign-in-widget/sign-in-widget.component';
 import { CardComponent } from './card/card.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, SignInWidgetComponent, CardComponent],
+  imports: [RouterOutlet, 
+    NavbarComponent, 
+    HeroComponent, 
+    SignInWidgetComponent, 
+    CardComponent, 
+    ToolsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
